820910fc17119a804432681a69bbbc413ad73862
Ernest <teluk.ernest@gmail.com>
2020-09-10 21:26:33 +0200

    config babel-plugin-macros to display styled-components name in dev mode

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components/macro';
+
+const SiteWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 function App() {
-  return <>silence is gold</>;
+  return <SiteWrapper>silence is gold</SiteWrapper>;
 }
 
 export default App;
